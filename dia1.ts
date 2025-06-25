4a429c6bcd94908833737591525764918f4701d9
ryanmarinhonjs <ryanmarinho@nasajon.com.br>
2025-06-25 20:47:14 -0300

    novos exercicios

--- dia1.ts
+++ dia1.ts
@@ -1,19 +1,61 @@
-interface User {
-  readonly id: number;
-  name: string;
-  role: "admin" | "user";
+// interface Users {
+//   id: number;
+//   name: string;
+//   role: "admin" | "user";
+// }
+
+// const users: Users[] = [
+//   { id: 1, name: "Ryan", role: "admin" },
+//   { id: 2, name: "Lara", role: "user" },
+//   { id: 3, name: "Jose", role: "user" },
+// ];
+
+// function getUserType(users: Users[]): string[] {
+//   const names: string[] = users.map((user: Users): string => user.name);
+//   return names;
+// }
+
+// function getUser(users: Users[]): void {
+//   const isAdmin = users.filter((user) => user.role === "admin");
+//   const isUser = users.filter((user) => user.role === "user");
+//   if (isAdmin.length > 0) {
+//     isAdmin.forEach((admin) => {
+//       console.log(`O usuário ${admin.name} é administrador`);
+//     });
+//   } else if (isUser.length > 0) {
+//     isUser.forEach((user) => {
+//       console.log(`O usuário ${user.name} não é administrador`);
+//     });
+//   }
+
+//   // return users.filter((user) => user.role === "admin");
+// }
+
+// getUser(users);
+
+// function somaNumber(number1: number, number2: number): number {
+//   return number1 + number2;
+// }
+
+// console.log(somaNumber(3, 5));
+
+interface ProdutosType {
+  id: number;
+  nome: string;
+  preco: number;
+  disponivel: boolean;
 }
 
-const useres: User[] = [
-  { id: 1, name: "Ryan", role: "admin" },
-  { id: 2, name: "Lara", role: "admin" },
-  { id: 3, name: "Jose", role: "user" },
+const productsBio: ProdutosType[] = [
+  { id: 1, nome: "Perfume", preco: 100.0, disponivel: true },
+  { id: 1, nome: "Batom", preco: 50.0, disponivel: true },
+  { id: 1, nome: "Gloss", preco: 20.0, disponivel: false },
 ];
 
-function getAdmin(useres: User[]): User[] {
-  // const filter = useres.filter(useres.role === "admin");
-  //   const filter = useres.filter((user) => user.role === "admin");
-  return useres.filter((user) => user.role === "admin");
+function getProducts(productsBio: ProdutosType[]): string[] {
+  const getProductsTrue = productsBio
+    .filter((productsBio) => productsBio.disponivel === true)
+    .map((productsBio) => productsBio.nome);
+  return getProductsTrue;
 }
-
-getAdmin(useres);
+console.log(getProducts(productsBio));
