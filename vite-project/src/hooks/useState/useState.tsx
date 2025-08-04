6f127fcb499405e30618cc089df2f43ddf71d4ee
ryanmarinhonjs <ryanmarinho@nasajon.com.br>
2025-08-04 15:28:24 -0300

    Estados com input controlados

--- vite-project/src/hooks/useState/useState.tsx
+++ vite-project/src/hooks/useState/useState.tsx
@@ -1,21 +1,49 @@
-import { useState } from 'react';
-
-interface FormularioInterface {
-  nome: string;
-  idade: number;
-  estaAtivo: boolean;
-}
+import { useState } from "react";
 
 function FormularioState() {
-  const [name, setName] = useState<string>('');
-  const [idade, setIdade] = useState<number>();
-  const [active, setActive] = useState<boolean>(true);
-
+  const [name, setName] = useState<string>("");
+  const [idade, setIdade] = useState<number | undefined>(0);
+  const [active, setActive] = useState<boolean>(false);
   return (
     <>
       <h1>Formulário Simples </h1>
-      <input type="text" />
-      <div className="font-bold">oi</div>
+      <div>
+        Nome:
+        <div className="bg-amber-600">
+          <input
+            type="text"
+            className="bg-teal-600"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div>{name}</div>
+        </div>
+      </div>
+      <div>
+        <span>Idade:</span>
+        <div className="bg-amber-500">
+          <input
+            type="number"
+            value={idade}
+            onChange={(e) => setIdade(Number(e.target.value))}
+            className="bg-teal-600"
+          />
+          <div>{`${idade} anos`}</div>
+        </div>
+      </div>
+
+      <div>
+        <span>Está ativo?</span>
+        <div className="bg-amber-500">
+          <div className="bg-teal-600">
+            <input
+              type="checkbox"
+              onChange={(e) => setActive(Boolean(e.target.checked))}
+            />
+            <div>{active ? "Sim" : "Não"}</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
